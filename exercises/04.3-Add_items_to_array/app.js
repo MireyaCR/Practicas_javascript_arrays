ae8cd3c69b169b74f2de9e4127868c937f06cebf
MireyaCR <mireya.mcr@gmail.com>
2022-08-23 18:25:37 +0000

    E4.4

--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -3,8 +3,7 @@ var arr = [4, 5, 734, 43, 45];
 // Your code here, use the push function and the random function.
 
 function getRandomInt() {
-    var randomNumber = Math.random();
-    var randomNumber = Math.floor(Math.random() * 1000);
+      var randomNumber = Math.floor(Math.random()* 1000);
     return randomNumber;
 }
 arr.push(getRandomInt(),getRandomInt());
--- exercises/04.4-Add_items_to_array_looping/app.js
+++ exercises/04.4-Add_items_to_array_looping/app.js
@@ -1,6 +1,14 @@
-var arr = [4,5,734,43,45];
+var arr = [4, 5, 734, 43, 45];
 
 //*****************
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 //*****************
+function getRandomInt() {
+    var randomNumber = Math.floor(Math.random() * 9) + 1;
+    return randomNumber;
+}
+for (var i = 0; i < 10; i++) {
+    arr.push(getRandomInt());
+}
+console.log(arr);
