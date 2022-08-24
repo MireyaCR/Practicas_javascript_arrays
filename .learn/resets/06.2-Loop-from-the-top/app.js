8c9941f0f5c981e6e1a55f0ad7882ba02897f661
MireyaCR <mireya.mcr@gmail.com>
2022-08-24 17:41:34 +0000

    E6,2

--- .learn/resets/06.2-Loop-from-the-top/app.js
+++ .learn/resets/06.2-Loop-from-the-top/app.js
@@ -1,6 +1,7 @@
 var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-var ultimo=[mySampleArray.length-1];
-for(var i = ultimo; i>-1; i=i-1){
+
+for(var i = 0; i<mySampleArray.length; i = i + 1)
+{
 	console.log(mySampleArray[i]);
 }
