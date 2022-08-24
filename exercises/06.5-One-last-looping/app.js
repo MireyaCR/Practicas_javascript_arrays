3459c18665fc05c775958d629d47da793d9584c9
MireyaCR <mireya.mcr@gmail.com>
2022-08-24 19:16:42 +0000

    6.5

--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,3 +1,8 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
+var ultimo = mySampleArray.length - 1;
+
+for (var index = ultimo; index >= 0; index--) {
+    console.log(mySampleArray[index]);
+}
