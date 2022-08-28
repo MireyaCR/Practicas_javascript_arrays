71f73829b6ff6ab77ab68cc2947d93405c7848af
MireyaCR <mireya.mcr@gmail.com>
2022-08-28 12:31:34 +0000

    E7,2

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,6 +1,16 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
-
 // your code here
 
+
+par = par.toLowerCase(); // convertir la cadena en minuscula
+par = par.replace(/\s/g, ''); // quitar espacios en blanco
+for (var i = 0; i < par.length; i++) {// recorro las letras
+    let letra = par[i];
+    if (counts[letra]) {
+        counts[letra]++;
+    } else {
+        counts[letra] = 1;
+    }
+  }
 console.log(counts);
