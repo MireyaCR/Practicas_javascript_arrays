0c9813c93c8af22a57f39eb716f9d110e1ef48a2
MireyaCR <mireya.mcr@gmail.com>
2022-08-28 12:50:37 +0000

    E7,3

--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,9 @@
-var arr = [45,67,87,23,5,32,60];
+var arr = [45, 67, 87, 23, 5, 32, 60];
 
 //Your code here.
+var ultimo = [arr.length-1];
+var nuevo = [];
+for (var i = ultimo; i >= 0; i--) {
+    nuevo.push(arr[i]);
+}
+console.log(nuevo);
