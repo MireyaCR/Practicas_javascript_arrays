057a7d5ae798ba585f60ed3f32f744b4830aabea
MireyaCR <mireya.mcr@gmail.com>
2022-08-29 17:40:29 +0000

    E8.1

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,8 @@
-var mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
+var mix = [42, true, "towel", [2, 1], 'hello', 34.4, { "name": "juan" }];
 
 //your code here
+
+var nuevo= mix.map(function(x){
+        return typeof(x);
+});
+console.log(nuevo);
