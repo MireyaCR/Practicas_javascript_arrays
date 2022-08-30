e3abc1c29912edde822718ed4204ca9035b1b724
MireyaCR <mireya.mcr@gmail.com>
2022-08-30 15:02:19 +0000

    E8.02

--- .learn/resets/08.2-Count-On/app.js
+++ .learn/resets/08.2-Count-On/app.js
@@ -4,9 +4,6 @@ let hello = [];
 for(let index = 0; index < myArray.length; index++){
     let element = myArray[index];
     // MAGIC HAPPENS HERE
-    if (typeof(element)=='object'){
-        hello.push(element);
-    }
 }
 
 console.log(hello)
