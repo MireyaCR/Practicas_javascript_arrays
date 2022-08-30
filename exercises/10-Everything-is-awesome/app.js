3720eb8921702a43a69d6d79734d629c79dd75fd
MireyaCR <mireya.mcr@gmail.com>
2022-08-30 17:10:31 +0000

    E10

--- exercises/10-Everything-is-awesome/app.js
+++ exercises/10-Everything-is-awesome/app.js
@@ -4,6 +4,14 @@ const ZerosToYahoos = (arr) => {
     let return_array = [];
     arr.forEach((item,index) => {
             // magic goes inside these brackets
+            switch (arr[index]) {
+                case 0:
+                    return_array.push('Yahoo');
+                    break;            
+                case 1:
+                    return_array.push(arr[index]);
+                    break;
+            }           
     });
     return return_array;
 };
