9b0b1fd575b981ddd2a587716a36c1768ff78da5
MireyaCR <mireya.mcr@gmail.com>
2022-09-29 19:22:42 +0000

    e12

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,7 +1,11 @@
 var people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
 //your code below
-
+function deletePerson(nombreal){
+    let newarr= people.filter((element) => element != nombreal)
+    return newarr
+}
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
 console.log(deletePerson('emilio'));
+
