2b9c0bdd058ed42398d0c7b71274ca610933fd21
MireyaCR <mireya.mcr@gmail.com>
2022-10-06 21:34:20 +0000

    arraysjavascript

--- exercises/20.2-More-mapping/app.js
+++ exercises/20.2-More-mapping/app.js
@@ -1,8 +1,12 @@
 
 
-var myNumbers = [23,234,345,4356234,243,43,56,2];
+var myNumbers = [23, 234, 345, 4356234, 243, 43, 56, 2];
 
 // your code here
-
-
-console.log(newArray);
+function myFunction(params) {
+    newArray = function(value) {
+    return value * 3;
+    }
+    myNumbers.map(newArray)
+}
+console.log(myFunction);
