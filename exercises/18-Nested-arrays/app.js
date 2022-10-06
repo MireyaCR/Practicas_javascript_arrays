2ff241543064c708aacc6bca62379f8667a16521
MireyaCR <mireya.mcr@gmail.com>
2022-10-06 15:57:16 +0000

    Practicasarrays

--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,1 +1,10 @@
 var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
+for (let i = 0; i < coordinatesArray.length; i++)
+     {
+        let subArray = coordinatesArray[i];
+        for (let j = 1; j < subArray.length; j++) {
+            console.log(subArray[j])
+    }
+  }
+  // Only change code above this line
+  
